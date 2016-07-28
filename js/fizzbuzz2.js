d299ed2914b8512daff23334d89994814faa6ee7
RussBus24 <rjhenry1014@gmail.com>
2016-07-28 14:06:29 -0400

    FizzBuzz Refactor JS file

--- js/fizzbuzz2.js
+++ js/fizzbuzz2.js
@@ -1,25 +1,54 @@
-var userChoice = prompt("Enter a number between 1 and 100.", "Type number here");
-console.log(userChoice);
+function userGetInput() {
 
-var userNumber = parseInt(userChoice);
-console.log(userNumber);
+	var userChoice = prompt("Enter a number between 1 and 100.", "Type number here");
+	console.log(userChoice);
 
-while(isNaN(userNumber)) {
-	
+	var userNumber = parseInt(userChoice);
+	console.log(userNumber);
+
+	while(isNaN(userNumber) || userNumber < 1 || userNumber > 100) {
+
+		userChoice = prompt("That is not a valid number. Please enter a number between 1 and 100.");
+		userNumber = parseInt(userChoice);
+	}
+	return userNumber;
 }
+
 
 function userResults(integer) {
 
 	var list = document.getElementById("list");
 	console.log(list);
 
-	for (var i = 0; i < integer.length; i++) {
+	for (var i = 0; i <= integer; i++) {
 		if (i % 3 === 0 && i % 5 === 0) {
-
+			var n = document.createElement("li");
+			n.textContent = "FizzBuzz!";
+			list.appendChild(n);
+			console.log("FizzBuzz!");
+		}
+		else if (i % 3 === 0) {
+			var o = document.createElement("li");
+			o.textContent = "Fizz!";
+			list.appendChild(o);
+			console.log("Fizz!");
+		}
+		else if (i % 5 === 0) {
+			var p = document.createElement("li");
+			p.textContent = "Buzz!";
+			list.appendChild(p);
+			console.log("Buzz!");
+		}
+		else {
+			var e = document.createElement("li");
+			e.textContent = i;
+			list.appendChild(e);
+			console.log(i);
 		}
 	}
-
 }
+var userNumber = userGetInput();
+userResults(userNumber);
 
 /*var list = document.getElementById("list");
 console.log(list);

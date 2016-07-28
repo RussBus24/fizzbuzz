222ad029d2f572053fb744967c3be00594a4fcc2
RussBus24 <rjhenry1014@gmail.com>
2016-07-28 14:00:18 -0400

    JavaScript File

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,17 +1,33 @@
-var list = document.getElementbyId
+var list = document.getElementById("list");
+console.log(list);
 
 	for(var i = 1; i <= 100; i++) {
-	if (i % 3 === 0 && i % 5 === 0) {
-		console.log("FizzBuzz!");
 
+	if (i % 3 === 0 && i % 5 === 0) {
+		var n = document.createElement("li");
+		n.textContent = "FizzBuzz!";
+		list.appendChild(n);
+		console.log("FizzBuzz!");
 	}
-	else if (i % 3 === 0 && i % 5 !== 0) {
+	else if (i % 3 === 0) {
+		var o = document.createElement("li");
+		o.textContent = "Fizz!";
+		list.appendChild(o);
 		console.log("Fizz!");
 	}
-	else if (i % 5 === 0 && i % 3 !== 0) {
+	else if (i % 5 === 0) {
+		var p = document.createElement("li");
+		p.textContent = "Buzz!";
+		list.appendChild(p);
 		console.log("Buzz!");
 	}
 	else {
+		var e = document.createElement("li");
+		e.textContent = i;
+		list.appendChild(e);
 		console.log(i);
 	}
+
 }
+
+
